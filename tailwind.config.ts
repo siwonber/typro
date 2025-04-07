1a919dba702c70ca48f75bc11804881432e9fbba
fhs50842 <fhs50842@fh-salzburg.ac.at>
2025-04-07 11:34:31 +0200

    setup with supabase done

---
+++ tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        accent: 'var(--color-accent)',
+        background: 'var(--color-background)',
+        surface: 'var(--color-surface)',
+        text: 'var(--color-text)',
+        muted: 'var(--color-muted)',
+        success: 'var(--color-success)',
+        error: 'var(--color-error)',
+        rank: {
+            bronze: 'var(--rank-bronze)',
+            silver: 'var(--rank-silver)',
+            gold: 'var(--rank-gold)',
+            platinum: 'var(--rank-platinum)',
+            diamond: 'var(--rank-diamond)',
+                }
+      },
+      fontFamily: {
+        sans: ['"Inter"', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
